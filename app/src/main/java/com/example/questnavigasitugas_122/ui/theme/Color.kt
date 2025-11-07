3cee55c4cf78c6c6fc8a1db802c5ebc1e6d20eda
ImronSatrioPurnomo <satriogames2223@gmail.com>
2025-11-07 23:12:34 +0700

    mengganti theme dan menambahkan color, serta menciptakan AppNavHost.kt

--- app/src/main/java/com/example/questnavigasitugas_122/ui/theme/Color.kt
+++ app/src/main/java/com/example/questnavigasitugas_122/ui/theme/Color.kt
@@ -9,3 +9,7 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Purple = Color(0xFF7C4DFF)
+val PurpleDark = Color(0xFF5E35B1)
+val PurpleLight = Color(0xFFD1C4E9)
--- app/src/main/java/com/example/questnavigasitugas_122/ui/theme/Theme.kt
+++ app/src/main/java/com/example/questnavigasitugas_122/ui/theme/Theme.kt
@@ -1,42 +1,30 @@
 package com.example.questnavigasitugas_122.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = PurpleDark,
+    secondary = Purple,
+    tertiary = PurpleLight
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
+    primary = Purple,
     onPrimary = Color.White,
+    secondary = PurpleDark,
     onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    surface = Color(0xFFF7F5FF),
+    surfaceVariant = PurpleLight
 )
 
 @Composable
-fun QuestNavigasiTugas_122Theme(
+fun QuestNavTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
@@ -45,7 +33,6 @@ fun QuestNavigasiTugas_122Theme(
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
-
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
