fd7f9c330cfe34594ab31746f63e0750e75a569d
ImronSatrioPurnomo <satriogames2223@gmail.com>
2025-11-07 23:19:53 +0700

    membuat Peserta.kt

---
+++ app/src/main/java/model/Peserta.kt
@@ -0,0 +1,10 @@
+//package model
+
+package com.example.questnavigasitugas_122.model
+
+data class Peserta(
+    val nama: String,
+    val jenisKelamin: String,
+    val statusPerkawinan: String,
+    val alamat: String
+)
